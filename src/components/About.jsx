69a7b57269b55d216871d63515139f4654c9856c
allentevyaw <atevyaw@gmail.com>
2023-01-04 19:56:26 -0500

    skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
                 <div></div>
             </div>
 
-            <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
+            <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
                 <div className='sm:text-right text-4xl  font-bold'>
                     <p>
                      Hi. I'm Allen, nice to meet you. Please take a look around.   
