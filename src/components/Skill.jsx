45abd4f1b500c1be937010caf1566ec80780e30e
allentevyaw <atevyaw@gmail.com>
2023-01-04 20:54:50 -0500

    skills finished

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -6,28 +6,55 @@ import Javascript from '../assets/javascript.png'
 import Node from '../assets/node.png'
 import ReactLogo from '../assets/react.png'
 import Tailwind from '../assets/tailwind.png'
+import HTML from '../assets/html.png'
 
 
 const Skill = () => {
   return (
-    <div name='skills' className='bg-[#0a192f] text-gray-300 '>
+    <div name='skills' className=' w-full h-screen bg-[#0a192f] text-gray-300 '>
 
         {/* Container */}
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p className='text-4xl font-bold border-b-4  border-pink-600'>Experience</p>
-                <p className='py-4'>These are the technologies that i work with</p>
+                <p className='text-4xl font-bold border-b-4  border-pink-600'>Skills</p>
+                <p className='py-4'>These are the technologies that I have experience working with</p>
             </div>
 
-            <div className='w-full grid grid-col-2 sm:grid-col-4 gap-4 text-center py-4'>
-                <div>
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-4'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-[80px] mx-auto' src={CSS} />
-                    <p>CSS</p>
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-[80px] mx-auto' src={Firebase} />
+                    <p className='my-4'>Firebase</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-[80px] mx-auto' src={Github} />
+                    <p className='my-4'>Github</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-[80px] mx-auto' src={Javascript} />
+                    <p className='my-4'>Javascript</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-[80px] mx-auto' src={Node} />
+                    <p className='my-4'>Node</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-[80px] mx-auto' src={ReactLogo} />
+                    <p className='my-4'>React</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-[80px] mx-auto' src={Tailwind} />
+                    <p className='my-4'>Tailwind</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-[80px] mx-auto' src={HTML} />
+                    <p className='my-4'>HTML</p>
                 </div>
             </div>
-
         </div>
-
     </div>
   )
 }
