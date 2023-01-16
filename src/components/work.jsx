afcb9ba1169db830bcdd4febff8a8efbba254aab
allentevyaw <atevyaw@gmail.com>
2023-01-16 18:59:14 -0500

    projects updated

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import NetflixImg from '../assets/netflix_build.png'
+import MemeImg from '../assets/meme-generator.png'
 
 const Work = () => {
   return (
@@ -19,7 +20,7 @@ const Work = () => {
                    {/* Hover Effects */}
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-[#EDEAEA] tracking-wider'>
-                            React JS Application
+                            Netflix Clone
                         </span>
                         <div className='pt-8 text-center'>
                             <a href='/'>
@@ -27,6 +28,26 @@ const Work = () => {
                                     Demo</button>
                             </a>
                             <a href='https://github.com/allentevyaw/Netflix-Clone'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div style={{backgroundImage: `url(${MemeImg})`}} 
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center mx-auto content-div items-center'>
+                   
+                   {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-[#EDEAEA] tracking-wider flex text-center'>
+                            Random Meme Generator
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Demo</button>
+                            </a>
+                            <a href='https://github.com/allentevyaw/Meme-generator'>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                                     Code</button>
                             </a>
