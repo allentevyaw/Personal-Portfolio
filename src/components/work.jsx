9e4e1ca3337e60ccb8c01708c5e1781e19403277
allentevyaw <atevyaw@gmail.com>
2023-01-22 20:33:14 -0500

    added work

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NetflixImg from '../assets/netflix_build.png'
 import MemeImg from '../assets/meme-generator.png'
+import Ecom from '../assets/ecom.png'
 
 const Work = () => {
   return (
@@ -48,6 +49,26 @@ const Work = () => {
                                     Demo</button>
                             </a>
                             <a href='https://github.com/allentevyaw/Meme-generator'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div style={{backgroundImage: `url(${Ecom})`}} 
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                   
+                   {/* Hover Effects */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <span className='text-2xl font-bold text-[#EDEAEA] tracking-wider'>
+                            Electronic
+                        </span>
+                        <div className='pt-8 text-center'>
+                            <a href='/'>
+                                <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+                                    Demo</button>
+                            </a>
+                            <a href='https://github.com/allentevyaw/Electronics-ecom'>
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                                     Code</button>
                             </a>
