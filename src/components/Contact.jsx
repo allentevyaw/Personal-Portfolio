bb64ae5e3e00214e3f5d57e90c62c1599e53bb62
allentevyaw <atevyaw@gmail.com>
2023-02-01 16:18:55 -0500

    button changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -13,7 +13,15 @@ const Contact = () => {
             <input className='p-2 bg-[#EDEAEA] rounded-lg' type='text' placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-[#EDEAEA] rounded-lg' type='email' placeholder='Email' name='email' />
             <textarea className='bg-[#EDEAEA] p-2 rounded-lg' name='message' rows='9' placeholder='Message'></textarea>
-            <button className='text-white border-2 bg-[#14B7B9] px-4 py-3 my-8 mx-auto flex items-center rounded-xl hover:opacity-75'>Send</button>
+            {/* <button className='text-white border-2 bg-[#14B7B9] px-4 py-3 my-8 mx-auto flex items-center rounded-xl hover:opacity-75'>Send</button> */}
+
+            <button class="text-[#EDEAEA] my-8 mx-auto shadow-md shadow-[#040c16] flex items-center p-0.5 overflow-hidden text-md font-medium rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+                        <span class="hover:text-white px-5 py-2.5 transition-all ease-in 
+                        duration-75 bg-[#1B2739] rounded-md group-hover:bg-opacity-0 ">
+                            Send
+                        </span>
+                </button>
+
             <div>
                 <p className='text-[#EDEAEA] text-sm hover:text-[#64FFDA] flex justify-center'>Designed & Built by Allen Tevyaw</p>
             </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
         
         {/* Container */}
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-2xl text-[#64FFDA]'>Hi, my name is</p>
+            <p className='text-2xl text-[#64FFDA]'>Welcome, my name is</p>
             <h1 className='pt-6 text-5xl sm:text-8xl font-bold text-[#EDEAEA]'>Allen Tevyaw</h1>
-            <h2 className='text-4xl sm:text-6xl font-bold text-[#1B2739]'>I'm a Full Stack Developer.</h2>
+            <h2 className='text-4xl sm:text-6xl font-bold text-[#1B2739]'>Full Stack Developer.</h2>
             <p className='pt-9 text-[#EDEAEA] py-4 max-w-[700px]'>
             As a full-stack developer, I bring a wealth of technical expertise and creative problem solving to the table. 
             With experience in React.js, Next.js, JavaScript, and iOS mobile applications, I am well-equipped to deliver high-quality, user-friendly web and mobile solutions.
@@ -25,21 +25,6 @@ const Home = () => {
                     </Link>
                     </span>
                 </button>
-
-                <button class="shadow-md shadow-[#040c16] flex items-center p-0.5 overflow-hidden text-md font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
-                >
-                        <span class=" px-5 py-2.5 transition-all ease-in duration-75 bg-[#1B2739] rounded-md group-hover:bg-opacity-0">
-                                <Link to="work" smooth={true} duration={500}>
-                            Check Out My Work
-                            <span className='group hover:rotate-90 duration-300 flex justify-center'>
-                                     <HiArrowNarrowRight className='ml-3'/>
-                            </span>
-                                </Link>
-                        </span>
-                </button>
-
-
-
             </div>
         </div>
     </div>
